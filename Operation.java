2c17895ffb089450c14ab2b5de0e1ca6684a153c
Souptek <144503115+Souptek@users.noreply.github.com>
2024-04-16 12:57:53 +0530

    Create Operation.java

---
+++ Operation.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*:
+class opr
+{
+   public static void main(String args[])
+   {
+      Scanner o=new Scanner(System.in);
+      System.out.println(" Enter the number");
+      int a=o.nextInt();
+      int b=o.nextInt();
+      int c=a+b;
+      system.out.println("addition"+c);
+   }
+}
